44f906d013573a55fac8fc02fa0a41c00268e546
mwoiii <147194899+mwoiii@users.noreply.github.com>
2025-09-18 23:25:37 +0100

    readme and thunderstore
    I guess also autoformatting

--- simple-lunar-coins/Compatibility.cs
+++ simple-lunar-coins/Compatibility.cs
@@ -1,24 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using RiskOfOptions;
 using RiskOfOptions.OptionConfigs;
 using RiskOfOptions.Options;
-using RiskOfOptions;
-using ProperSave;
 using UnityEngine;
 
-namespace SimpleLunarCoins
-{
-    public static class RiskOfOptionsCompatibility
-    {
+namespace SimpleLunarCoins {
+    public static class RiskOfOptionsCompatibility {
         private static bool? _enabled;
 
-        public static bool enabled
-        {
-            get
-            {
-                if (_enabled == null)
-                {
+        public static bool enabled {
+            get {
+                if (_enabled == null) {
                     _enabled = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions");
                 }
                 return (bool)_enabled;
@@ -26,8 +20,7 @@ namespace SimpleLunarCoins
         }
 
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        public static void OptionsInit()
-        {
+        public static void OptionsInit() {
             ModSettingsManager.AddOption(new SliderOption(SimpleLunarCoins.coinChance, new SliderConfig() { min = 0, max = 100 }));
             ModSettingsManager.AddOption(new StepSliderOption(SimpleLunarCoins.coinMultiplier, new StepSliderConfig() { min = 0, max = 1, increment = 0.01f }));
             ModSettingsManager.AddOption(new IntSliderOption(SimpleLunarCoins.startingCoins, new IntSliderConfig() { min = 0, max = 10000 }));
@@ -42,16 +35,12 @@ namespace SimpleLunarCoins
         }
     }
 
-    public static class ProperSaveCompatibility
-    {
+    public static class ProperSaveCompatibility {
         private static bool? _enabled;
 
-        public static bool enabled
-        {
-            get
-            {
-                if (_enabled == null)
-                {
+        public static bool enabled {
+            get {
+                if (_enabled == null) {
                     _enabled = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.KingEnderBrine.ProperSave");
                 }
                 return (bool)_enabled;
@@ -59,30 +48,25 @@ namespace SimpleLunarCoins
         }
 
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        public static void AddEvent(Action<Dictionary<string, object>> action)
-        {
+        public static void AddEvent(Action<Dictionary<string, object>> action) {
             ProperSave.SaveFile.OnGatherSaveData += action;
         }
 
-        public static bool IsLoading
-        {
+        public static bool IsLoading {
             [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
             get { return ProperSave.Loading.IsLoading; }
         }
 
         // forgive me for my foolish ways this whole compatibility thing has gotten me in a kerfuffle it's 3:30am and the birds are tweeting I'm going insane
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        public static string GetModdedData(string name)
-        {
+        public static string GetModdedData(string name) {
             return ProperSave.Loading.CurrentSave.GetModdedData<string>(name);
         }
     }
 
-    public class SimpleLunarCoins_ProperSaveObj
-    {
+    public class SimpleLunarCoins_ProperSaveObj {
         public Dictionary<ulong, uint> playerCoins;
-        public SimpleLunarCoins_ProperSaveObj(Dictionary<ulong, uint> playerCoins)
-        {
+        public SimpleLunarCoins_ProperSaveObj(Dictionary<ulong, uint> playerCoins) {
             this.playerCoins = playerCoins;
         }
     }
